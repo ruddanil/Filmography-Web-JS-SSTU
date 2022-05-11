4aa139cc618ab35e73ebed6a391b4d64b5695e5b
Даниил Рудь <ruddanil99@gmail.com>
2022-05-11 17:48:40 +0400

    Final

--- modules/localStorageHelper.js
+++
@@ -1,7 +0,0 @@
-function getFilmList (localStorage) {
-    let filmListJson = localStorage.getItem(this.localStorage);
-    if (filmListJson !== null) {
-        return JSON.parse(filmListJson);
-    }
-    return [];
-}
